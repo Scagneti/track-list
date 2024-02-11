6fff877c8066d07f66aaa30ecc57b6c65621dff7
Zach <scagneti@protonmail.com>
2024-02-11 11:12:26 -0500

    logic for reading mp3 tags for each file in a directory

--- TrackList/TrackList/Program.cs
+++ TrackList/TrackList/Program.cs
@@ -2,13 +2,37 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Enter the file path of your album or collection of songs:");
+        Console.Write("Enter the file path of your album or collection of songs:");
 
-        var filePath = Console.ReadLine();
+        var filePath = Console.ReadLine() ?? string.Empty;
 
-        foreach (var file in Directory.GetFiles(filePath))
+        if (filePath != string.Empty)
         {
-            Console.WriteLine(file);
+            var startingTime = "00:00:00";
+
+            List<TagLib.Tag> fileTags = new();
+
+            foreach (var file in Directory.GetFiles(filePath))
+            {
+                var audioFile = TagLib.File.Create(file);
+                var tagCollection = TagLib.File.Create(file).Tag;
+
+                tagCollection.Length = tagCollection.Length ?? audioFile.Properties.Duration.ToString();
+
+                fileTags.Add(tagCollection);
+            }
+
+            fileTags.OrderBy(ft => ft.Track).ToList().ForEach(tag =>
+            {
+                var trackNumber = tag.Track;
+                var title = tag.Title;
+                var trackLength = tag.Length;
+
+                if (trackNumber == 1)
+                {
+
+                }
+            });
         }
     }
 }
