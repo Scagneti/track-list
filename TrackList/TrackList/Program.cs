06a77c5b1faa1d0202809103a4e5c78782064a17
Zach <scagneti@protonmail.com>
2024-02-11 10:24:31 -0500

    initial commit

---
+++ TrackList/TrackList/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the file path of your album or collection of songs:");
+
+        var filePath = Console.ReadLine();
+
+        foreach (var file in Directory.GetFiles(filePath))
+        {
+            Console.WriteLine(file);
+        }
+    }
+}
