f8ed655d56b1fecfec7d2a6faa242b9606b7dd64
Zach Nelson <scagneti@protonmail.com>
2024-03-16 16:49:29 -0400

    extracting logic into methods and refactoring

--- TrackList/TrackList/Program.cs
+++ TrackList/TrackList/Program.cs
@@ -1,13 +1,19 @@
-﻿internal class Program
+﻿using TagLib;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
+        string[] audioExtensions = { ".mp3", ".wav", ".flac", ".ogg" };
         string filePath = "";
         bool validPath = false;
+        List<Tag> fileTags = new();
+        TimeSpan accumulatedTime = TimeSpan.Zero;
 
         while (!validPath)
         {
             Console.Write("Enter the file path of your album or collection of songs:");
+
             filePath = Console.ReadLine() ?? string.Empty;
 
             if (!string.IsNullOrEmpty(filePath) && Directory.Exists(filePath))
@@ -20,39 +26,43 @@
             }
         }
 
-        TimeSpan accumulatedTime = TimeSpan.Zero;
-
-        List<TagLib.Tag> fileTags = new();
-
-        string[] audioExtensions = { ".mp3", ".wav", ".flac", ".ogg" };
-
         foreach (var file in Directory.GetFiles(filePath))
         {
-            if(audioExtensions.Contains(Path.GetExtension(file)))
+            if (audioExtensions.Contains(Path.GetExtension(file)))
             {
-                var audioFile = TagLib.File.Create(file);
-                var tagCollection = audioFile.Tag;
-
-                tagCollection.Length = tagCollection.Length ?? audioFile.Properties.Duration.ToString();
-
-                fileTags.Add(tagCollection);
-            }
+                var tag = GetFileTags(audioExtensions, file);
+                fileTags.Add(tag);
+            };
         }
 
+        CreateTrackList(fileTags, accumulatedTime);
+
+        Console.WriteLine("Track List Successfully Displayed! Press Any Key To Exit.");
+        Console.Read();
+    }
+
+    private static void CreateTrackList(List<Tag> fileTags, TimeSpan accumulatedTime)
+    {
         fileTags.OrderBy(ft => ft.Track).ToList().ForEach(tag =>
         {
             var trackNumber = tag.Track;
             var title = tag.Title;
             var trackLength = tag.Length;
-
             var songStart = accumulatedTime.ToString(@"hh\:mm\:ss");
 
             accumulatedTime = accumulatedTime.Add(TimeSpan.Parse(trackLength));
 
             Console.WriteLine($"{songStart} {title}");
         });
+    }
 
-        Console.WriteLine("Track List Successfully Displayed! Press Any Key To Exit.");
-        Console.Read();
+    private static Tag GetFileTags(string[] audioExtensions, string file)
+    {       
+        var audioFile = TagLib.File.Create(file);
+        var audioFileTags = audioFile.Tag;
+
+        audioFileTags.Length = audioFileTags.Length ?? audioFile.Properties.Duration.ToString();
+
+        return audioFileTags;
     }
 }
